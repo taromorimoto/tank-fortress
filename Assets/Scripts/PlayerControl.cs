fbcc7b56038273325b3c71d7e549c79d00db582c
Taro Morimoto <taro.morimoto@gmail.com>
2014-11-18 11:28:24 +0200

    Turn and move to clicked target on a map.

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,8 +4,10 @@ using System.Collections;
 public class PlayerControl : MonoBehaviour {
 	public GameObject goTerrain;
 	public float turnSpeed = 1.0f;
-
+	public float driveSpeed = 1.0f;
+	
 	bool turning = false;
+	bool driving = false;
 	Vector3 target;
 	
 	void Update() {
@@ -19,11 +21,41 @@ public class PlayerControl : MonoBehaviour {
 		}
 		
 		if (turning) {
+			Vector3 forward = new Vector3(transform.forward.x, 0, transform.forward.z);
 			Vector3 targetDir = target - transform.position;
-			float step = turnSpeed * Time.deltaTime;
-			Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0F);
-			Debug.DrawRay(transform.position, newDir, Color.red);
-			transform.rotation = Quaternion.LookRotation(newDir);
+			targetDir.y = 0;
+			targetDir.Normalize();
+			forward.Normalize();
+			
+			if (Vector3.Angle(targetDir, forward) > 5.0F) {
+				Vector3 newDir = Vector3.RotateTowards(forward, targetDir, turnSpeed * Time.deltaTime, 0.0F);
+				Debug.DrawRay(transform.position, newDir, Color.red);
+				newDir.y = transform.forward.y;
+				print (transform.rotation.eulerAngles.y + "   " + Vector3.Angle(targetDir, forward));
+				newDir.Normalize();
+				transform.rotation = Quaternion.LookRotation(newDir);
+				//transform.LookAt(target);
+				//transform.Rotate(0, turnSpeed * Time.deltaTime, 0);
+			} else {
+				print("Target closer than 5 degrees, stopping turning.");
+				turning = false;
+				driving = true;
+			}
+		}
+		
+		if (driving) {
+			float dist = Vector3.Distance(target, transform.position);
+			if (dist > 1) {
+				transform.position = Vector3.MoveTowards(transform.position, target, driveSpeed * Time.deltaTime);
+				
+			} else {
+				print("Target closer than 1, stopping driving.");
+				driving = false;
+			}
 		}
 	}
 }
+
+
+
+
