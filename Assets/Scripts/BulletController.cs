057923e88ba472caa65fb8272899607ef0640a62
Taro Morimoto <taro.morimoto@gmail.com>
2014-11-18 12:10:28 +0200

    Fortress can shoot with a cannon.

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletController : MonoBehaviour {
+
+    public float age = 1.0f;
+    public float ageAfterHit = 0.1f;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+        age -= Time.deltaTime;
+
+        if (age < 0) {
+            Destroy(gameObject);
+        }
+	}
+
+    void OnTriggerEnter(Collider other) {
+        age = ageAfterHit;
+        Light light = transform.FindChild("Point light").GetComponent<Light>();
+        light.intensity *= 2;
+    }
+}
---
+++ Assets/Scripts/CannonController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CannonController : MonoBehaviour {
+
+    public Transform launchPoint;
+	public GameObject bullet;
+    public float bulletVelocity = 1000;
+    public float rateOfFire = 0.1f;
+
+    float lastShot = 0;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+        lastShot += Time.deltaTime;
+
+        bool fireKeyDown = Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow);
+
+        if (fireKeyDown) {
+            if (lastShot > rateOfFire) {
+                Fire();
+                lastShot = 0;
+            }
+        }		
+	
+	}
+
+    void Fire() {
+        GameObject bulletInstance = (GameObject)Instantiate(bullet, launchPoint.position, launchPoint.rotation);
+        bulletInstance.GetComponent<Rigidbody>().AddForce(launchPoint.forward * bulletVelocity);
+    }
+
+
+}
