45aa6553a1827d3bbe3bd35c8edb1208d6892392
Taro Morimoto <taro.morimoto@gmail.com>
2014-12-09 15:05:18 +0200

    Health bar UI for tanks

--- Assets/Scripts/HealthControl.cs
+++ Assets/Scripts/HealthControl.cs
@@ -1,12 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class HealthControl : MonoBehaviour {
 
+	public Slider healthBarSlider;
 	public float health = 100.0f;
 
-	void Start () {
+	float maxHealth;
 	
+	void Start () {
+		maxHealth = health;
 	}
 	
 	void Update () {
@@ -15,6 +19,7 @@ public class HealthControl : MonoBehaviour {
 	
 	public void ApplyDamage(float damage) {
 		health -= damage;
+		healthBarSlider.value -= damage / maxHealth;
 		print ("Fortress hit. Damage: " + damage + " Health:" + health);
 		if (health < 0) {
 			// Make death animation here
