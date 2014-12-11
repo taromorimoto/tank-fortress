e5a40ff9b06dfc2a760982290ce7f2601da59094
Taro Morimoto <taro.morimoto@gmail.com>
2014-12-11 14:50:19 +0200

    Tweaked aim

--- Assets/Scripts/CannonController.cs
+++ Assets/Scripts/CannonController.cs
@@ -14,6 +14,7 @@ public class CannonController : MonoBehaviour {
 	public float baseCharge = 0.3f;
 	public float energyUsePerShot = 0.1f;
 	public float energyRegenerationSpeed = 0.03f;
+	public float aimTweak = 60.0f;
 	
 	GameObject aim;
 	bool charging = false;
@@ -26,7 +27,9 @@ public class CannonController : MonoBehaviour {
 	}
 
 	Vector3 GetAimPosition() {
-		return transform.parent.position + transform.parent.forward * (baseCharge + charge) * 70.0f;
+		float c = (baseCharge + charge);
+		float v = c * Mathf.Pow(0.95f, 0.5f) * aimTweak;
+		return transform.parent.position + transform.parent.forward * v;
 	}
 	
 	public void UseNitro() {
