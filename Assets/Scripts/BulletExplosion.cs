ac873e5b40f38bb082d76810a272fd0818a729a5
Taro Morimoto <taro.morimoto@gmail.com>
2014-12-11 17:01:54 +0200

    Variation to bullet explosions

---
+++ Assets/Scripts/BulletExplosion.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletExplosion : MonoBehaviour {
+
+	public AudioClip[] explosions;
+	AudioSource aud;
+	
+	// Use this for initialization
+	void Start () {
+		int i = Random.Range(0, explosions.Length);
+		aud = (AudioSource)gameObject.GetComponent<AudioSource>();
+		aud.clip = explosions[i];
+		print(i);
+		aud.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
