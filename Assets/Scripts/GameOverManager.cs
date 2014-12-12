30b59715b5ebdb5ef7431bb55d048c6dde57207e
Taro Morimoto <taro.morimoto@gmail.com>
2014-12-12 16:32:29 +0200

    Game over now detected and reloads the Battle scene

---
+++ Assets/Scripts/GameOverManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOverManager : MonoBehaviour {
+
+	void FixedUpdate () {
+		if (HasGameEnded()) {
+			// Add here delay to load and show the winning player text etc! :)
+			Application.LoadLevel("Battle");
+		}
+	}
+	
+	bool HasGameEnded() {
+		GameObject[] tanks = GameObject.FindGameObjectsWithTag("Tank");
+		if (tanks.Length == 1) {
+			print("Winner: " + tanks[0].name);
+			return true;
+		} else if (tanks.Length == 0) {
+			print("Both Players died!");
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
