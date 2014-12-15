4f1d98b7debc5d8f5f481d2630db4e12ce4af5dc
unknown <ruonala1@A802-POGAME09.org.aalto.fi>
2014-12-15 21:06:50 +0200

    final fixes

--- Assets/Scripts/IntroScript.cs
+++ Assets/Scripts/IntroScript.cs
@@ -5,6 +5,7 @@ public class IntroScript : MonoBehaviour {
 
 	public AudioSource hitSound;
 	public float hitSoundPlayDelay = 2.0f;
+	public float reloadDelay = 0.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -15,6 +16,11 @@ public class IntroScript : MonoBehaviour {
 	void Update () {
 		if(Input.anyKey){
 			Application.LoadLevel (1);
+		}
+
+		reloadDelay += Time.deltaTime;
+		if (reloadDelay > 60 * 5) {
+			Application.LoadLevel(0);
 		}
 	}
 }
