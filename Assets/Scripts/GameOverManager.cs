3e67ef82717e1bd54f75cc4db4e0e1ad5cd80550
Taro Morimoto <taro.morimoto@gmail.com>
2014-12-15 20:40:41 +0200

    "The Winner is" text shows up

--- Assets/Scripts/GameOverManager.cs
+++ Assets/Scripts/GameOverManager.cs
@@ -1,13 +1,17 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class GameOverManager : MonoBehaviour {
 
+	public GameObject gameOverText;
 	bool spacePressed = false;
+	string text = "";
 	
 	void FixedUpdate () {
 		if (HasGameEnded()) {
-			// Add here delay to load and show the winning player text etc! :)
+			gameOverText.GetComponent<Text>().text = text;
+			gameOverText.SetActive(true);
 			
 			if (spacePressed) {
 				Application.LoadLevel("Intro");
@@ -24,11 +28,11 @@ public class GameOverManager : MonoBehaviour {
 	bool HasGameEnded() {
 		GameObject[] tanks = GameObject.FindGameObjectsWithTag("Tank");
 		if (tanks.Length == 1) {
-			print("Winner: " + tanks[0].name);
+			text = "THE WINNER IS " + tanks[0].name;
 			audio.Stop();
 			return true;
 		} else if (tanks.Length == 0) {
-			print("Both Players died!");
+			text = "BOTH PLAYERS DIE!";
 			audio.Stop();
 			return true;
 		} else {
