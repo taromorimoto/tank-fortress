9f6e110446073d8573df3c9a2faa8bcbb9a288fe
Taro Morimoto <taro.morimoto@gmail.com>
2014-12-15 19:54:53 +0200

    Damage and healthkits show up on screen

---
+++ Assets/Scripts/DamageText.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DamageText : MonoBehaviour {
+	
+	void Start () {
+		Destroy(gameObject, 2.0f);
+	}
+	
+	void Update () {
+		transform.position += new Vector3(0, 20.0f * Time.deltaTime, 0);
+	}
+	
+	public void SetText(string text, Color color) {
+		Text tc = GetComponent<Text>();
+		tc.text = text;
+		tc.color = color;
+	}
+}
