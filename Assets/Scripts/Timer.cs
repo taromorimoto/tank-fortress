69b51d66c471905d7d64704d587ce84945e26503
Taro Morimoto <taro.morimoto@gmail.com>
2014-12-16 09:54:17 +0200

    Timer component to deactivate and destroy

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+	public float deactivateAfter;
+	public float destroyAfter;
+	
+	void Update () {
+		
+		if (deactivateAfter > 0) {
+			deactivateAfter -= Time.deltaTime;
+			if (deactivateAfter <= 0) {
+				gameObject.SetActive(false);
+			}
+		}
+		
+		if (destroyAfter > 0) {
+			destroyAfter -= Time.deltaTime;
+			if (destroyAfter <= 0) {
+				Destroy(gameObject);
+			}
+		}
+	}
+}
